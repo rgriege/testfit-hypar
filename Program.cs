555d5ac1506949539067b667610cee6b80b05c82
rgriege <rgriege@sbcglobal.net>
2018-07-01 09:38:14 -0500

    Replace libtest with libtfmp

--- Program.cs
+++ Program.cs
@@ -3,14 +3,25 @@ using System.Runtime.InteropServices;
 
 namespace dotnet_test
 {
+    struct v2f
+    {
+        public float x;
+        public float y;
+    }
+
     class Program
     {
-        [DllImport("libtest.so")]
-        public static extern void show_message();
+        [DllImport("libtfmp.so")]
+        public static extern void tfmp_generate_bldg(v2f[] boundary, uint n,
+                                                     float mass_width,
+                                                     float mass_height,
+                                                     float aspect_ratio);
 
         static void Main(string[] args)
         {
-            show_message();
+            v2f[] boundary = { new v2f{ x=5.0f, y=42.0f } };
+            tfmp_generate_bldg(boundary, (uint)boundary.Length,
+                               60.0f, 40.0f, 0.5f);
         }
     }
 }
--- makefile
+++ makefile
@@ -1,6 +1,14 @@
-libtest.so: test.c
-	gcc -fPIC -shared test.c -o libtest.so
+CC      := gcc
+CCFLAGS := -std=gnu99
+CCFLAGS += -Wall -Werror -Wno-missing-braces -Wno-unused-local-typedefs -Wshadow
+CCFLAGS += -DAPP_CODENAME=\"tfmp\"
+
+HEADERS := testfit/ctyd.h testfit/debug.h testfit/mass_placement.h testfit/measure.h testfit/mp_utility.h testfit/opt.h testfit/opts.h testfit/poly.h testfit/types.h testfit/utility.h
+SOURCES := testfit/mass_placement.c testfit/measure.c testfit/mp_utility.c testfit/opt.c testfit/opts.c testfit/poly.c testfit/utility.c tfmp.c
+
+libtfmp.so: $(HEADERS) $(SOURCES)
+	$(CC) -fPIC -shared -I. $(CCFLAGS) $(SOURCES) -o libtfmp.so
 
 .PHONY: clean
 clean:
-	rm -f libtest.so
+	rm -f libtfmp.so
