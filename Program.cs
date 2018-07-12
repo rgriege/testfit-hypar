44f84a46e2257dcb761318872400d200def8e635
rgriege <rgriege@sbcglobal.net>
2018-07-12 16:40:34 -0500

    Output result to gltf using Hypar SDK

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Hypar.Elements;
+using Hypar.Geometry;
 
 namespace dotnet_test
 {
@@ -8,6 +11,21 @@ namespace dotnet_test
     {
         public float x;
         public float y;
+    }
+
+    class MeshElement : Proxy
+    {
+        private Mesh _mesh;
+
+        public MeshElement(Mesh mesh) : base(null, null)
+        {
+            this._mesh = mesh;
+        }
+
+        public override Mesh Tessellate()
+        {
+            return this._mesh;
+        }
     }
 
     class Program
@@ -34,47 +52,46 @@ namespace dotnet_test
                 new v2f{ x=200.0f, y=200.0f },
                 new v2f{ x=0.0f,   y=200.0f },
             };
-						IntPtr poly_verts_;
-						IntPtr poly_lengths_;
+						IntPtr poly_verts;
+						IntPtr poly_lengths;
 						uint num_verts;
 						uint num_lengths;
 						int ret;
 
             ret = tfmp_generate_bldg(boundary, (uint)boundary.Length,
                                      60.0f, 40.0f, 0.5f,
-                                     out poly_verts_, out num_verts,
-                                     out poly_lengths_, out num_lengths);
+                                     out poly_verts, out num_verts,
+                                     out poly_lengths, out num_lengths);
 
             if (ret != 0) {
                 Console.WriteLine("tfmp_generate_bldg() failed");
                 return;
             }
 
-            v2f[] poly_verts = new v2f[num_verts];
-            uint[] poly_lengths = new uint[num_lengths];
-						for (int i = 0; i < num_verts; ++i)
-						{
-							IntPtr poly_vert_ = IntPtr.Add(poly_verts_, i * Marshal.SizeOf<v2f>());
-							poly_verts[i] = Marshal.PtrToStructure<v2f>(poly_vert_);
-						}
-						for (int i = 0; i < num_lengths; ++i)
-						{
-							IntPtr poly_length_ = IntPtr.Add(poly_lengths_, i * sizeof(uint));
-							poly_lengths[i] = Marshal.PtrToStructure<uint>(poly_length_);
-						}
-
-            tfmp_free(poly_verts_, poly_lengths_);
-
-            uint vert_start = 0;
-						for (uint i = 0; i < num_lengths; ++i)
+            List<Polyline> polylines = new List<Polyline>();
+            int poly_vert_start = 0;
+            for (int i = 0; i < num_lengths; ++i)
             {
-                Console.Write("Polygon: ");
-                for (uint j = 0; j < poly_lengths[i]; ++j)
-                    Console.Write("<{0},{1}>,", poly_verts[vert_start+j].x,
-                                  poly_verts[vert_start+j].y);
-                Console.Write("\n");
-                vert_start += poly_lengths[i];
+                IntPtr lengthp = IntPtr.Add(poly_lengths, i * sizeof(uint));
+                int length = (int)Marshal.PtrToStructure<uint>(lengthp);
+                List<Vector3> verts = new List<Vector3>();
+                for (int j = 0; j < length; ++j)
+                {
+                    IntPtr vertp = IntPtr.Add(poly_verts, (poly_vert_start + j) * Marshal.SizeOf<v2f>());
+                    v2f vert = Marshal.PtrToStructure<v2f>(vertp);
+                    verts.Add(new Vector3(vert.x, vert.y));
+                }
+                verts.Reverse();
+                polylines.Add(new Polyline(verts));
+                poly_vert_start += length;
             }
+
+            tfmp_free(poly_verts, poly_lengths);
+
+            var model = new Model();
+            var mesh = new MeshElement(Mesh.Extrude(polylines, 10.0));
+            model.AddElement(mesh);
+            model.SaveGlb("test.glb");
         }
     }
 }
